ad870beae1d834904570dd378aab9c18047c18eb
Andhika Prasetya <andhikapr081@gmail.com>
2024-03-10 12:07:40 +0700

    feat: testing when user input and submit the form ( failed )

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import user from '@testing-library/react';
+import userEvent from '@testing-library/react';
 import UserForm from './UserForm';
 
 test('shows two inputs and a button', () => {
@@ -13,4 +13,30 @@ test('shows two inputs and a button', () => {
   // assertion - make sure the component is doing what we expect it to do
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
+});
+
+test('should call onUserAdd callback when the form is submitting', async () => {
+  // 1. not the best implementation
+
+  // try to render component
+  render(<UserForm />);
+
+  // Find two inputs
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+  // simulate typing a name
+  await userEvent.click(nameInput);
+  await userEvent.keyboard('jane');
+
+  // simulate typing an email
+  await userEvent.click(emailInput);
+  await userEvent.keyboard('jane@email.com');
+
+  // find the button
+  const button = screen.getByRole('button');
+
+  // simulate clicking submit button
+  userEvent.click(button);
+
+  // assertion - make sure the component is doing what we expect it to do
 });
