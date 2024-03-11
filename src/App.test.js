c951bd583750a679257bc3a1a9c81fd835d1fb05
Andhika Prasetya <andhikapr081@gmail.com>
2024-03-11 19:50:25 +0700

    feat : test to see if new user is in the list after submit create user

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('can receive new user and display it in the list', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByRole('textbox', {
+    name: /name/i,
+  });
+
+  const emailInput = screen.getByRole('textbox', {
+    name: /email/i,
+  });
+
+  const submitButton = screen.getByRole('button');
+
+  // user enter name
+  await userEvent.click(nameInput);
+  await userEvent.keyboard('hendry');
+
+  // user enter email
+  await userEvent.click(emailInput);
+  await userEvent.keyboard('hendry@email.com');
+
+  // user click submit button to add new list
+  await userEvent.click(submitButton);
+
+  // get the new name
+  const newNameList = screen.getByRole('cell', {
+    name: 'hendry',
+  });
+
+  // get the new email
+  const newEmailList = screen.getByRole('cell', {
+    name: 'hendry@email.com',
+  });
+
+  // Assertion -> expect the name and email to be in the list after user submitting form
+  expect(newNameList).toBeInTheDocument();
+  expect(newEmailList).toBeInTheDocument();
 });
--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import UserList from './UserList';
 
 test('render one row per user', () => {
