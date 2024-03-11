397a70eaf2c460cf5673a6a83d5ed59db6d3827a
Andhika Prasetya <andhikapr081@gmail.com>
2024-03-11 10:01:15 +0700

    feat : writing test to get all rows in the table

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,1 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import UserList from './UserList';
+
+test('render one row per user', () => {
+  // render component
+  const users = [
+    {
+      name: 'jane',
+      email: 'jane@email.com',
+    },
+    {
+      name: 'sam',
+      email: 'sam@email.com',
+    },
+  ];
+  render(<UserList users={users} />);
+
+  // find all rows in the table
+  const rows = screen.getAllByRole('row');
+
+  expect(rows).toHaveLength(2);
+});
